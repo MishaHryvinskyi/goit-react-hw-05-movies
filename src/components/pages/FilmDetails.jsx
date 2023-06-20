aebaebf3ad9603a4e735ddc2a0821bd8a8653c1c
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-06-20 22:22:38 +0300

    router

--- src/components/pages/FilmDetails.jsx
+++ src/components/pages/FilmDetails.jsx
@@ -1,9 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link, useLocation } from "react-router-dom";
+import { useRef } from "react";
 
 const FilmDitalis = () => {
     const { filmsId } = useParams();
+    const location = useLocation();
+    const backLinkLocationRef = useRef(location.state?.from ?? '/films');
     console.log('отаке:', filmsId)
-    return <div>Деталі про фільм: {filmsId}</div>
+    console.log(location);
+    return <div>
+            <h1>Деталі про фільм: {filmsId}</h1>
+            <Link to={backLinkLocationRef.current}>Назад до сторінки колекція</Link>
+        </div>
 }
 
 export default FilmDitalis;
--- src/components/pages/Films.jsx
+++ src/components/pages/Films.jsx
@@ -1,12 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
+import { useState } from "react";
+
 
 const Films = () => {
-    const films = [ "film-1", "film-2", "film-3", "film-4", "film-5" ];
-    return <div>{films.map(film => {
-        return <Link key={film} to={`${film}`}>
+const [films, setFilms] = useState([ "film-1", "film-2", "film-3", "film-4", "film-5" ]);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const location = useLocation();
+    const filmsId = searchParams.get('filmsId') ?? '';
+    
+    const updateQueryString = e => {
+        const filmIdValue = e.target.value;
+        if(filmIdValue === '') {
+            return setSearchParams({});
+        } 
+        setSearchParams({ filmsId: filmIdValue });
+    }
+
+    const visibleFilms = films.filter(film => film.includes(filmsId))
+
+    console.log(location)
+    return <div>
+        <input type="text" onChange={updateQueryString}/>
+        <ul>
+        {visibleFilms.map(film => {
+        return (
+            <li>
+                <Link key={film} state={{ from: location }} to={`${film}`}>
             {film}
         </Link>
-    })}</div>
+            </li>
+        )
+    })}
+        </ul>
+        </div>
 }
 
 export default Films;
