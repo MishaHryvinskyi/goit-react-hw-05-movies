112dd29f4d2710ad0ccf562411870e756832967b
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-06-22 19:18:45 +0300

    movieDetails

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,16 +1,72 @@
 import { useParams, Link, useLocation } from "react-router-dom";
-import { useRef } from "react";
+import { useEffect, useState, useRef } from "react";
+import {
+  getMoviesById,
+  getCast,
+  getReviews,
+} from "../components/API/API"; // Підключаємо функції отримання даних з сервера
 
 const MovieDetails = () => {
-    const { movieId } = useParams();
-    const location = useLocation();
-    const backLinkLocationRef = useRef(location.state?.from ?? '/movies');
-    console.log('отаке:', movieId)
-    console.log(location);
-    return <div>
-            <h1>Деталі про фільм: {movieId}</h1>
-            <Link to={backLinkLocationRef.current}>Назад до сторінки колекція</Link>
+  const { movieId } = useParams();
+  const location = useLocation();
+  const backLinkLocationRef = useRef(location.state?.from ?? "/movies");
+  const [movieDetails, setMovieDetails] = useState(null);
+  const [cast, setCast] = useState([]);
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const movieResponse = await getMoviesById(movieId);
+        setMovieDetails(movieResponse.data);
+      } catch (error) {
+        console.log("Error fetching movie details:", error);
+      }
+    };
+
+    const fetchCast = async () => {
+      try {
+        const castResponse = await getCast(movieId);
+        setCast(castResponse.data.cast);
+      } catch (error) {
+        console.log("Error fetching cast:", error);
+      }
+    };
+
+    const fetchReviews = async () => {
+      try {
+        const reviewsResponse = await getReviews(movieId);
+        setReviews(reviewsResponse.data.results);
+      } catch (error) {
+        console.log("Error fetching reviews:", error);
+      }
+    };
+
+    fetchMovieDetails();
+    fetchCast();
+    fetchReviews();
+  }, [movieId]);
+
+  return (
+    <div>
+      {movieDetails ? (
+        <div>
+          <h1>Деталі про фільм: {movieDetails.title}</h1>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
+            alt={movieDetails.title}
+          />
+          <p>Overview: {movieDetails.overview}</p>
+          <p>Рейтинг: {movieDetails.vote_average}</p>
+          <p>Дата виходу: {movieDetails.release_date}</p>
+          
         </div>
-}
+      ) : (
+        <p>Loading...</p>
+      )}
+      <Link to={backLinkLocationRef.current}>Назад до сторінки колекція</Link>
+    </div>
+  );
+};
 
 export default MovieDetails;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,38 +1,49 @@
 import { Link, useLocation, useSearchParams } from "react-router-dom";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { getSearchMovies } from "../components/API/API";
 
 const Movies = () => {
-const [movies, setMovies] = useState([ "movie-1", "movie-2", "movie-3", "movie-4", "movie-5" ]);
-    const [searchParams, setSearchParams] = useSearchParams();
-    const location = useLocation();
-    const movieId = searchParams.get('movieId') ?? '';
-    
-    const updateQueryString = e => {
-        const movieIdValue = e.target.value;
-        if(movieIdValue === '') {
-            return setSearchParams({});
-        } 
-        setSearchParams({ movieId: movieIdValue });
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  const movieId = searchParams.get("movieId") ?? "";
+
+  const updateQueryString = (e) => {
+    const movieIdValue = e.target.value;
+    if (movieIdValue === "") {
+      return setSearchParams({});
     }
+    setSearchParams({ movieId: movieIdValue });
+  };
 
-    const visibleMovies = movies.filter(movie => movie.includes(movieId))
+  useEffect(() => {
+    const searchMovies = async () => {
+      try {
+        const response = await getSearchMovies(movieId);
+        const moviesData = response.data.results;
+        setMovies(moviesData);
+      } catch (error) {
+        console.error("Error searching movies:", error);
+      }
+    };
 
-    console.log(location)
-    return <div>
-        <input type="text" onChange={updateQueryString}/>
-        <ul>
-        {visibleMovies.map(movie => {
-        return (
-            <li>
-                <Link key={movie} state={{ from: location }} to={`${movie}`}>
-            {movie}
-        </Link>
-            </li>
-        )
-    })}
-        </ul>
-        </div>
-}
+    searchMovies();
+  }, [movieId]);
+
+  return (
+    <div>
+      <input type="text" onChange={updateQueryString} />
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <Link state={{ from: location }} to={`/movies/${movie.id}`}>
+              {movie.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Movies;
